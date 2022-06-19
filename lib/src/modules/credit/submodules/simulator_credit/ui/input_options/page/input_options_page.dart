4fbb3bff8c5aa2f3943bc5a25a5bd915a6ef77d0
Arthur Barbosa <arthur.bos@hotmail.com>
2022-06-19 18:04:37 -0300

    Initial Commit

--- lib/src/modules/credit/submodules/simulator_credit/ui/input_options/page/input_options_page.dart
+++ lib/src/modules/credit/submodules/simulator_credit/ui/input_options/page/input_options_page.dart
@@ -42,6 +42,7 @@ class _InputOptionsPageState extends State<InputOptionsPage> {
               controller.onChangeLtv(value);
             },
           ),
+          ElevatedButton(onPressed: () {}, child: const Text('Continuar'))
         ],
       ),
     );
